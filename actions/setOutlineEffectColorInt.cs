cc9730679acfa89a4a2e9d9f7ae15c2874faa583
Eric Vander Wal <eric_vanderwal@hotmail.com>
2017-04-19 21:11:21 +0800

    Add files via upload

---
+++ actions/setOutlineEffectColorInt.cs
@@ -0,0 +1,69 @@
+﻿// Custom Action by DumbGameDev
+// www.dumbgamedev.com
+
+using UnityEngine;
+using cakeslice;
+
+namespace HutongGames.PlayMaker.Actions
+{
+    [ActionCategory("Outline Effect")]
+    [Tooltip("Set outline effect color int on object.")]
+
+    public class setOutlineEffectColorInt : FsmStateAction
+
+    {
+        [RequiredField]
+        [CheckForComponent(typeof(Outline))]
+        public FsmOwnerDefault gameObject;
+
+        [Title("Color Int")]
+        public FsmInt colorInt;
+        public FsmBool everyFrame;
+
+        Outline theScript;
+
+        public override void Reset()
+        {
+
+            colorInt = null;
+            gameObject = null;
+            everyFrame = false;
+        }
+
+        public override void OnEnter()
+        {
+            var go = Fsm.GetOwnerDefaultTarget(gameObject);
+
+            theScript = go.GetComponent<Outline>();
+
+            if (!everyFrame.Value)
+            {
+                MakeItSo();
+                Finish();
+            }
+
+        }
+
+        public override void OnUpdate()
+        {
+            if (everyFrame.Value)
+            {
+                MakeItSo();
+            }
+        }
+
+
+        void MakeItSo()
+        {
+            var go = Fsm.GetOwnerDefaultTarget(gameObject);
+            if (go == null)
+            {
+                return;
+            }
+
+            theScript.color = colorInt.Value;
+
+        }
+
+    }
+}
